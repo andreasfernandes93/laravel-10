7b3d4dce540114638082bbad25cdbd2d3b4e4c35
Andreas Fernandes <andreas.sf93@gmail.com>
2024-03-28 20:06:57 -0300

    feat: add show, edit, update and destroy routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\Admin\{TicketController};
  
-
 Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
 Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
 Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+Route::get('/tickets/{id}/edit', [TicketController::class, 'edit'])->name('tickets.edit');
+Route::put('/tickets/{id}', [TicketController::class, 'update'])->name('tickets.update');
+Route::delete('/tickets/{id}', [TicketController::class, 'destroy'])->name('tickets.destroy');
 
 // Routes for testing
 Route::get('/contact', [SiteController::class, 'contact']);
