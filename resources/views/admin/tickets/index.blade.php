131474e644358313fae7c50de37b0f5f5ab39a6c
Andreas Fernandes <andreas.sf93@gmail.com>
2024-03-28 20:06:08 -0300

    feat: add show and edit link

--- resources/views/admin/tickets/index.blade.php
+++ resources/views/admin/tickets/index.blade.php
@@ -18,6 +18,10 @@
             <td>{{ $ticket->subject }}</td>
             <td>{{ $ticket->status }}</td>
             <td>{{ $ticket->message }}</td>
+            <td>
+                <a href="{{ route('tickets.show', $ticket->id) }}">Ver</a>
+                <a href="{{ route('tickets.edit', $ticket->id) }}">Editar</a>
+            </td>
         </tr>
          @endforeach 
     </tbody>
